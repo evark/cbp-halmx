c6a5d8cf0f7eb22b72840024966980745c8171e3
EvArk <arkalis.e@gmail.com>
2016-07-20 17:36:57 +0300

    fixes

--- tests/blink1/main.cpp
+++ tests/blink1/main.cpp
@@ -1,9 +1,11 @@
 #include <cmsis.h>
 
 #ifdef STM32F103xB
+/* BLUE PILL */
     #define LEDPORT GPIOC
     #define LEDPIN  GPIO_PIN_13
 #else
+/* NUCLEO */
     #define LEDPORT GPIOA
     #define LEDPIN  GPIO_PIN_5
 #endif
@@ -21,17 +23,17 @@ int main(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LEDPORT, LEDPIN, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PtPin */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Pin = LEDPIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(LEDPORT, &GPIO_InitStruct);
 
     while (1)
   {
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_GPIO_TogglePin(LEDPORT, LEDPIN);
 		for (int i = 0; i < 800000; i++)	/* Wait a bit. */
 			__asm__("nop");
   }
--- tests/blink2/main.cpp
+++ tests/blink2/main.cpp
@@ -1,28 +1,37 @@
 #include <cmsis.h>
 
-/// LED on Bluepill and Nucleo
-
 #ifdef STM32F103xB
+/* BLUE PILL */
     #define LEDPORT GPIOC
     #define LEDPIN  GPIO_PIN_13
 #else
+/* NUCLEO */
     #define LEDPORT GPIOA
     #define LEDPIN  GPIO_PIN_5
 #endif
 
+
 int main(void)
 {
-  GPIO_InitTypeDef  GPIO_InitStruct;
+  GPIO_InitTypeDef GPIO_InitStruct;
 
-   __HAL_RCC_GPIOC_CLK_ENABLE();
-  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull  = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LEDPORT, LEDPIN, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = LEDPIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LEDPORT, &GPIO_InitStruct);
   while (1)
   {
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_GPIO_TogglePin(LEDPORT, LEDPIN);
     HAL_Delay(500);
   }
 }
