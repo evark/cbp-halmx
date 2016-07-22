be7d2c0a2a5252628f249b0dad40bf973a393edf
EvArk <arkalis.e@gmail.com>
2016-07-22 18:30:06 +0300

    Support for building examples from Arduino IDE. Directories reordering.

---
+++ stm32/libraries/CBP_HALMX_EXAMPLES/examples/Digital/Blink1/Blink1.ino
@@ -0,0 +1,45 @@
+/*
+
+ Simple Blink Test with loop delay
+
+*/
+
+
+#ifdef STM32F103xB
+/* BLUE PILL */
+    #define LEDPORT GPIOC
+    #define LEDPIN  GPIO_PIN_13
+#else
+/* NUCLEO */
+    #define LEDPORT GPIOA
+    #define LEDPIN  GPIO_PIN_5
+#endif
+
+
+int main(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LEDPORT, LEDPIN, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = LEDPIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LEDPORT, &GPIO_InitStruct);
+
+    while (1)
+  {
+        HAL_GPIO_TogglePin(LEDPORT, LEDPIN);
+		for (int i = 0; i < 800000; i++)	/* Wait a bit. */
+			__asm__("nop");
+  }
+}
