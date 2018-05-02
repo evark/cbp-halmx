0cc4286e474335a2ffa64b93c58856c7b8aa90ed
EvArk <arkalis.e@gmail.com>
2018-05-02 19:37:56 +0300

    Toolchain Updates

--- stm32/cores/stm32hal/itoa.h
+++ stm32/cores/stm32hal/itoa.h
@@ -24,9 +24,9 @@ extern "C"{
 
 //extern void itoa( int n, char s[] ) ;
 
-extern char* itoa( int value, char *string, int radix ) ;
+//extern char* itoa( int value, char *string, int radix ) ;
 extern char* ltoa( long value, char *string, int radix ) ;
-extern char* utoa( unsigned long value, char *string, int radix ) ;
+//extern char* utoa( unsigned long value, char *string, int radix ) ;
 extern char* ultoa( unsigned long value, char *string, int radix ) ;
 
 #ifdef __cplusplus
